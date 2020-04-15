64bb0467c1edec1c5fd2d407a7009d14046cd5da
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-04-15 16:02:41 -0500

    fixing linter errors

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,16 @@
+function initialize() {
+  var latitude = new google.maps.LatLng(40.650002, -73.949997);
+  var zoomCenter = {
+    zoom: 2,
+    center: latitude,
+  };
+  var map = new google.maps.Map(document.getElementById("map"), zoomCenter);
 
+  var marker = new google.maps.Marker({
+    position: lattitude,
+    map: map,
+    title: "festival location",
+  });
+}
+
+google.maps.event.addDomListener(window, "load", initialize);
