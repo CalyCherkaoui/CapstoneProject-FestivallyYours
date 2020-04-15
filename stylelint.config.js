7093217c4f6ba81234f779b9cfebca139c87efc3
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-04-15 18:38:07 -0500

    fixing linter errors

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,13 +3,13 @@
 module.exports = {
   "extends": "stylelint-config-recommended",
   "rules": {
-    "at-rule-empty-line-before": [ "always", {
+    "at-rule-empty-line-before": ["always", {
       except: [
         "blockless-after-same-name-blockless",
         "first-nested",
       ],
       ignore: ["after-comment"],
-    } ],
+    }],
     "at-rule-name-case": "lower",
     "at-rule-name-space-after": "always-single-line",
     "at-rule-semicolon-newline-after": "always",
@@ -22,12 +22,12 @@ module.exports = {
     "block-opening-brace-space-before": "always",
     "color-hex-case": "lower",
     "color-hex-length": "short",
-    "comment-empty-line-before": [ "always", {
+    "comment-empty-line-before": ["always", {
       except: ["first-nested"],
       ignore: ["stylelint-commands"],
-    } ],
+    }],
     "comment-whitespace-inside": "always",
-    "custom-property-empty-line-before": [ "always", {
+    "custom-property-empty-line-before": ["always", {
       except: [
         "after-custom-property",
         "first-nested",
@@ -36,7 +36,7 @@ module.exports = {
         "after-comment",
         "inside-single-line-block",
       ],
-    } ],
+    }],
     "declaration-bang-space-after": "never",
     "declaration-bang-space-before": "always",
     "declaration-block-semicolon-newline-after": "always-multi-line",
@@ -47,7 +47,7 @@ module.exports = {
     "declaration-colon-newline-after": "always-multi-line",
     "declaration-colon-space-after": "always-single-line",
     "declaration-colon-space-before": "never",
-    "declaration-empty-line-before": [ "always", {
+    "declaration-empty-line-before": ["always", {
       except: [
         "after-declaration",
         "first-nested",
@@ -56,7 +56,7 @@ module.exports = {
         "after-comment",
         "inside-single-line-block",
       ],
-    } ],
+    }],
     "function-comma-newline-after": "always-multi-line",
     "function-comma-space-after": "always-single-line",
     "function-comma-space-before": "never",
@@ -82,10 +82,10 @@ module.exports = {
     "number-leading-zero": "always",
     "number-no-trailing-zeros": true,
     "property-case": "lower",
-    "rule-empty-line-before": [ "always-multi-line", {
+    "rule-empty-line-before": ["always-multi-line", {
       except: ["first-nested"],
       ignore: ["after-comment"],
-    } ],
+    }],
     "selector-attribute-brackets-space-inside": "never",
     "selector-attribute-operator-space-after": "never",
     "selector-attribute-operator-space-before": "never",
