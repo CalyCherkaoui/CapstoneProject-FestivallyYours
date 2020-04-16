f03b4d4f00df0b9ed8edb48bd21636c7bd14e3ce
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-04-16 17:41:38 -0500

    fixing at-rule error

--- stylelint.config.js
+++ stylelint.config.js
@@ -10,6 +10,9 @@ module.exports = {
       ],
       ignore: ["after-comment"],
     }],
+    "at-rule-no-unknown": [true, {
+      "ignoreAtRules": ["function", "if", "each", "include", "mixin"]
+    }],
     "at-rule-name-case": "lower",
     "at-rule-name-space-after": "always-single-line",
     "at-rule-semicolon-newline-after": "always",
